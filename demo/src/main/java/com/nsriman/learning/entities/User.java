f0102f2baebeb25d480ac543327161770d8007be
Sriman Nerella <147592395+nsrimanmj@users.noreply.github.com>
2026-02-04 10:20:01 +0000

    Add entities

---
+++ demo/src/main/java/com/nsriman/learning/entities/User.java
@@ -0,0 +1,25 @@
+package com.nsriman.learning.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // That's it! No manual getters or setters needed.
+}
